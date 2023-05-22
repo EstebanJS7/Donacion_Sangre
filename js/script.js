c03c7aee648a1e039fde92b80c0641b4e16ebbf4
Esteban Jimenez <estebandjs7@gmail.com>
2023-05-22 08:46:55 -0400

    actualizacion interfaz

--- js/script.js
+++ js/script.js
@@ -7,63 +7,39 @@ $('#btn-cargar').on('click', function (evento) {
             // Generar el HTML con tu diseño para cada elemento
             let tipo_sangre = ["A+", "A-", "B+", "B-", "O+", "O-", "AB-", "AB+"];
             let source = "img/" + tipo_sangre[elemento.tipo_sangre - 1] + ".png";
-            var html =
-                '<div class="tablas border border-2 rounded-3 w-50 bg-white mt-5">' +
-                '<div class="row">' +
-                '<div class="col-6">' +
-                '<label class="form-label"><strong>' + elemento.nombre_apellido_donatario + '</strong></label>' +
-                '</div>' +
-                '<div class="col-6">' +
-                '<p class="text-end"><i class="bi bi-trash-fill"></i>' + '<i class="bi bi-share text-danger"></i></p>' +
-                '</div>' +
-                '<div>' +
-
-                '<div class="row">' +
-                '<div class="col-6">' +
-                '<label class="form-label">Telefono: </label>' +
-                '</div>' +
-                '<div class="col-6">' +
-                '<p class="text-end">' + elemento.telefono_contacto + '</p>' +
-                '</div>' +
-                '<div>' +
-                '<div class="row">' +
-                '<div class="col-6">' +
-                '<label class="form-label">C.I: </label>' +
-                '</div>' +
-                '<div class="col-6">' +
-                '<p class="text-end">' + elemento.cedula_donatario + '</p>' +
-                '</div>' +
-                '<div>' +
-                '<div class="row">' +
-                '<div class="col-6">' +
-                '<label class="form-label">Lugar de donacion: </label>' +
-                '</div>' +
-                '<div class="col-6">' +
-                '<p class="text-end">' + elemento.establecimiento + '</p>' +
-                '</div>' +
-                '<div>' +
-                '<div class="row">' +
-                '<div class="col-6">' +
-                '<label class="form-label">R.H: </label>' +
-                '</div>' +
-                '<div class="col-6">' +
-                '<p class="text-end">' + elemento.tipo_sangre + '</p>' +
-                '</div>' +
-                '<div>' +
-                '<div class="row">' +
-                '<div class="col-6">' +
-                '<label class="form-label">Volumenes: </label>' +
-                '</div>' +
-                '<div class="col-6">' +
-                '<p class="text-end">' + elemento.volumenes_necesarios + '</p>' +
-                '</div>' +
-                '<div>' +
-                '<div class="row">' +
-                '<div class="col-12">' +
-                '<p class="text-center">' + elemento.solicitud + '</p>' +
-                '</div>' +
-                '<div>' +
-                '</div>'
+            var html =  ` <div class="border border-2 rounded-3 w-50 bg-white mx-auto mt-5 mb-2 p-2">
+            <div class="row">
+  
+              <p class="d-flex justify-content-between">
+                <span><strong>${elemento.nombre_apellido_donatario}</strong></span>
+                <span><i class="bi bi-trash-fill"></i><i class="bi bi-share text-danger"></i></span>
+              </p>
+              <p class="d-flex justify-content-between">
+                <span>Telefono: </span>
+                <span>${elemento.telefono_contacto}</span>
+              </p>
+              <p class="d-flex justify-content-between">
+                <span>C.I: </span>
+                <span>${elemento.cedula_donatario}</span>
+              </p>
+              <p class="d-flex justify-content-between">
+                <span>Lugar de donacion: </span>
+                <span>${elemento.establecimiento}</span>
+              </p>
+              <p class="d-flex justify-content-between">
+                <span>R.H: </span>
+                <span>${elemento.tipo_sangre}</span>
+              </p>
+              <p class="d-flex justify-content-between">
+                <span>Volumenes: </span>
+                <span>${elemento.volumenes_necesarios}</span>
+              </p>
+              <p class="text-center">
+                ${elemento.solicitud}
+              </p>
+              
+            </div>
+          </div>`
 
             // Agregar el HTML generado al contenedor
             $('.container').append(html);
